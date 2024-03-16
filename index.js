f5baaab5882eba9998b52c72720477c9166e2cd8
Maxim Tyunin <you@example.com>
2024-03-16 13:44:02 +0300

    update filter

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getCount(str) {
     const arr = str.split('')
-    const arr2 = arr.filter((letter) => letter == 'a' || letter == 'e' || letter == 'i' || letter == 'u' || letter == 'o')
+    const arr2 = arr.filter((letter) => 'aouei'.includes(letter))
     return arr2.length;
 }
 console.log(getCount("aatagxueujaifjdgiodf"))
